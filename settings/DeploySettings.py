ecab836b001d41d315555c746328c64207f79a96
Coulibaly <coulibalysadama@gmail.com>
2020-11-12 03:02:55 +0000

    Initial commit

---
+++ settings/DeploySettings.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+#PATH_TO_MODEL_CHECKPOINTS = "data/bestResult/G2D19_P2OF_ResHB_1LSTM/NoDataAug/save_epoch_12/ViolenceNet.ckpt"
+#PATH_TO_MODEL_CHECKPOINTS = "temp/G2D19_P2OF_ResHB_1LSTM_dataAug_expLR/save_epoch_2"
+PATH_TO_MODEL_CHECKPOINTS = "./temp/model/save_epoch_10/ViolenceNet.ckpt"
+'''
+      To smooth the Judgement of Fight/NoneFight, count of neighbor frames
+    (that has the value) should >= CHANGE_JUDGEMENT_THRESHOLD to change the
+    judgement.
+      Usually, this value is setted to the same as the frames Threshold that
+    produce the Best Accuracy in Validation set.
+
+    ex:
+	NetOutput	Judgement
+	  False		  False
+	  True		  False
+	  True		  False
+	  True		  True
+	  False		  True
+	  Flase		  True
+	  Flase		  False
+'''
+CHANGE_JUDGEMENT_THRESHOLD = 3
+
+DISPLAY_IMAGE_SIZE = 400
+
+BORDER_SIZE = 40
+FIGHT_BORDER_COLOR = (0, 0, 255)
+NO_FIGHT_BORDER_COLOR = (0, 255, 0)
